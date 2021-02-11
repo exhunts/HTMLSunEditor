efe7df34411c5d0e13090502f3f22ad339f7792a
gregorykafanov <gregkaf@gmail.com>
2021-02-11 21:48:12 +0200

    (ref): HTMLEditor moved to separate component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,4 @@
-import { Editor } from '@tinymce/tinymce-react'
-import { useEffect, useRef, useState } from 'react'
+import HTMLEditor from '../HTMLEditor/HTMLEditor'
 import './App.css'
 
 // Optimization
@@ -11,43 +10,10 @@ import './App.css'
 // New Idea :   Get rid of 'useState hook' and use just 'useRef' instead.
 // ------------
 function App() {
-  const [initialValue, setInitialValue] = useState('')
-  const refStateToSave = useRef('')
-
-  const handleEditorChange = (content, editor) => {
-    refStateToSave.current = content
-  }
-
-  const onUnload = () => {
-    localStorage.setItem('_htmleditor_', refStateToSave.current)
-  }
-
-  useEffect(() => {
-    window.addEventListener('beforeunload', onUnload)
-    setInitialValue(localStorage.getItem('_htmleditor_'))
-    refStateToSave.current = localStorage.getItem('_htmleditor_')
-    return () => window.removeEventListener('beforeunload', onUnload)
-  }, [])
-
   return (
-    <Editor
-      apiKey="hh5kg0elochz6bff3y37wn287nnbefh3g07o68m5fl6igvay"
-      initialValue={initialValue}
-      init={{
-        height: 500,
-        menubar: true,
-        plugins: [
-          'advlist autolink lists link image charmap print preview anchor',
-          'searchreplace visualblocks code fullscreen',
-          'insertdatetime media table paste code help wordcount',
-        ],
-        toolbar:
-          'undo redo | formatselect | bold italic backcolor | \
-          alignleft aligncenter alignright alignjustify | \
-          bullist numlist outdent indent | removeformat | help',
-      }}
-      onEditorChange={handleEditorChange}
-    />
+    <>
+      <HTMLEditor />
+    </>
   )
 }
 
---
+++ src/components/HTMLEditor/HTMLEditor.jsx
@@ -0,0 +1,44 @@
+import { Editor } from '@tinymce/tinymce-react'
+import { useEffect, useRef, useState } from 'react'
+
+export default function HTMLEditor() {
+  const [initialValue, setInitialValue] = useState('')
+  const refStateToSave = useRef('')
+
+  const handleEditorChange = (content, editor) => {
+    refStateToSave.current = content
+  }
+
+  const onUnload = () => {
+    localStorage.setItem('_htmleditor_', refStateToSave.current)
+  }
+
+  useEffect(() => {
+    window.addEventListener('beforeunload', onUnload)
+    const localStorageState = localStorage.getItem('_htmleditor_')
+    setInitialValue(localStorageState)
+    refStateToSave.current = localStorageState
+    return () => window.removeEventListener('beforeunload', onUnload)
+  }, [])
+
+  return (
+    <Editor
+      apiKey="hh5kg0elochz6bff3y37wn287nnbefh3g07o68m5fl6igvay"
+      initialValue={initialValue}
+      init={{
+        height: 500,
+        menubar: true,
+        plugins: [
+          'advlist autolink lists link image charmap print preview anchor',
+          'searchreplace visualblocks code fullscreen',
+          'insertdatetime media table paste code help wordcount',
+        ],
+        toolbar:
+          'undo redo | formatselect | bold italic backcolor | \
+        alignleft aligncenter alignright alignjustify | \
+        bullist numlist outdent indent | removeformat | help',
+      }}
+      onEditorChange={handleEditorChange}
+    />
+  )
+}
