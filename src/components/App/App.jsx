db86e37b6a800c7fb2d57fd86ebf13eb87d02081
gregorykafanov <gregkaf@gmail.com>
2021-02-11 22:21:04 +0200

    (ref): delete comments

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,14 +3,6 @@ import HTMLEditor from '../HTMLEditor/HTMLEditor'
 import Jumbotrone from '../Jumbotrone/Jumbotrone'
 import './App.css'
 
-// Optimization
-// ------------
-// Question :   What is to optimize?
-// Answer   :   Every editor change fires 'handleEditorChange' that
-//              triggers 'localStorage' update.
-// Solution :   On leaving page show alert with prompt to save file in 'localStorage'.
-// New Idea :   Get rid of 'useState hook' and use just 'useRef' instead.
-// ------------
 function App() {
   return (
     <>
