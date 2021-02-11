e5fd684ca83755efea63693e98ae0e1046992665
gregorykafanov <gregkaf@gmail.com>
2021-02-11 19:37:50 +0200

    (feat): tinymce integrated

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,31 @@
+import { Editor } from '@tinymce/tinymce-react'
 import './App.css'
 
 function App() {
-  return <div className="App">Hello</div>
+  const handleEditorChange = (content, editor) => {
+    console.log('Content was updated:', content)
+  }
+
+  return (
+    <Editor
+      apiKey="hh5kg0elochz6bff3y37wn287nnbefh3g07o68m5fl6igvay"
+      initialValue="<p>This is the initial content of the editor</p>"
+      init={{
+        height: 500,
+        menubar: true,
+        plugins: [
+          'advlist autolink lists link image charmap print preview anchor',
+          'searchreplace visualblocks code fullscreen',
+          'insertdatetime media table paste code help wordcount',
+        ],
+        toolbar:
+          'undo redo | formatselect | bold italic backcolor | \
+        alignleft aligncenter alignright alignjustify | \
+        bullist numlist outdent indent | removeformat | help',
+      }}
+      onEditorChange={handleEditorChange}
+    />
+  )
 }
 
 export default App
