470ad28e2ee54769f864ffe16617130a8d734553
gregorykafanov <gregkaf@gmail.com>
2021-02-12 13:52:37 +0200

    (ref): refactor

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import Footer from './Footer'
+import HTMLEditor from './HTMLEditor'
+import Jumbotrone from './Jumbotrone'
+
+function App() {
+  return (
+    <>
+      <Jumbotrone />
+      <HTMLEditor />
+      <Footer />
+    </>
+  )
+}
+
+export default App
